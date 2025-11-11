1cf41cd635eebe69d90f608a77129e30fc19a4f8
DanXuuuuu <139434572+DanXuuuuu@users.noreply.github.com>
2025-11-10 18:25:33 -0800

    Connection and Discover Profile page

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -69,11 +69,27 @@ const Connections = () => {
                   }
                   {
                     currentTab === 'Following' && (
-                      <button className=''>
+                      <button className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer'>
                         Unfollow
                       </button>
                     )
                   }
+                  {
+                    currentTab === 'Pending' && (
+                      <button className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer'>
+                        Accept
+                      </button>
+                    )
+                  }
+                                    {
+                    currentTab === 'Connections' && (
+                      <button onClick={()=>navigate(`/messages/${user._id}`)} className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer flex items-center justify-center gap-1'>
+                        <MessagesSquare className='w-4 h-4'/>
+                        Message
+                      </button>
+                    )
+                  }
+                  
                   </div>
                 </div>
             </div>
